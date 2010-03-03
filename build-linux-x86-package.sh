fbd863bbe44e54094b890e6cac2e8bb3a90ecb72
kgraefe <>
2010-03-03 14:46:04 +0000

    * Version 1.4 * Completed french translation

--- build-linux-x86-package.sh
+++ build-linux-x86-package.sh
@@ -8,6 +8,7 @@ mkdir -p ${DIR}/pixmaps/pidgin/${PROJECT} && \
 mkdir -p ${DIR}/sounds/pidgin/${PROJECT} && \
 mkdir -p ${DIR}/locale/de/LC_MESSAGES && \
 mkdir -p ${DIR}/locale/ru/LC_MESSAGES && \
+mkdir -p ${DIR}/locale/fr/LC_MESSAGES && \
 cp ChangeLog ${DIR}/ChangeLog && \
 cp README.linux_binary ${DIR}/ReadMe && \
 cp src/.libs/${PROJECT}.so ${DIR}/plugins && \
@@ -15,6 +16,7 @@ cp share/pixmaps/*.png ${DIR}/pixmaps/pidgin/${PROJECT} && \
 cp share/sounds/*.wav ${DIR}/sounds/pidgin/${PROJECT} && \
 cp po/de.gmo ${DIR}/locale/de/LC_MESSAGES/${PROJECT}.mo && \
 cp po/ru.gmo ${DIR}/locale/ru/LC_MESSAGES/${PROJECT}.mo && \
+cp po/fr.gmo ${DIR}/locale/fr/LC_MESSAGES/${PROJECT}.mo && \
 cp install_linux_package.sh ${DIR}/install.sh && \
 tar czvf ${DIR}.tar.gz ${DIR} && \
 rm -rf ${DIR}
--- build-win32-package.sh
+++ build-win32-package.sh
@@ -8,6 +8,7 @@ mkdir -p ${WIN32DIR}/pidgin/pixmaps/pidgin/${PROJECT} && \
 mkdir -p ${WIN32DIR}/pidgin/sounds/pidgin/${PROJECT} && \
 mkdir -p ${WIN32DIR}/pidgin/locale/de/LC_MESSAGES && \
 mkdir -p ${WIN32DIR}/pidgin/locale/ru/LC_MESSAGES && \
+mkdir -p ${WIN32DIR}/pidgin/locale/fr/LC_MESSAGES && \
 sed 's/$/\r/' ChangeLog >${WIN32DIR}/ChangeLog.txt && \
 sed 's/$/\r/' README.win32 >${WIN32DIR}/ReadMe.txt && \
 cp src/${PROJECT}.dll ${WIN32DIR}/pidgin/plugins && \
@@ -16,6 +17,7 @@ cp share/sounds/*.wav ${WIN32DIR}/pidgin/sounds/pidgin/${PROJECT} && \
 i586-mingw32msvc-strip --strip-unneeded ${WIN32DIR}/pidgin/plugins/${PROJECT}.dll && \
 cp po/de.gmo ${WIN32DIR}/pidgin/locale/de/LC_MESSAGES/${PROJECT}.mo && \
 cp po/ru.gmo ${WIN32DIR}/pidgin/locale/ru/LC_MESSAGES/${PROJECT}.mo && \
+cp po/fr.gmo ${WIN32DIR}/pidgin/locale/fr/LC_MESSAGES/${PROJECT}.mo && \
 rm -f ${WIN32DIR}.zip && \
 cd ${WIN32DIR} && \
 zip -r ../${WIN32DIR}.zip * && \
