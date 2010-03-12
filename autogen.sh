47a15af9d2a82644e5af052ff096512bed46b381
kgraefe <>
2010-03-12 12:22:25 +0000

    * added spain language file

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,7 @@ aclocal \
 && autoheader \
 && automake --add-missing \
 && autoconf \
+&& libtoolize --copy --force --install \
 && ((intltoolize --version) < /dev/null > /dev/null 2>&1 || {
     echo;
     echo "You must have intltool installed to compile birthday reminder!";
