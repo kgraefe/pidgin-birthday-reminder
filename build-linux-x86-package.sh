72c4b6b39463cc490f4075c24b94becafb636fc7
kgraefe <>
2010-03-12 12:38:59 +0000

    * updated spain language header and build files

--- build-linux-x86-package.sh
+++ build-linux-x86-package.sh
@@ -6,17 +6,15 @@ DIR=${PROJECT}-`cat VERSION`-linux-x86 && \
 mkdir -p ${DIR}/plugins && \
 mkdir -p ${DIR}/pixmaps/pidgin/${PROJECT} && \
 mkdir -p ${DIR}/sounds/pidgin/${PROJECT} && \
-mkdir -p ${DIR}/locale/de/LC_MESSAGES && \
-mkdir -p ${DIR}/locale/ru/LC_MESSAGES && \
-mkdir -p ${DIR}/locale/fr/LC_MESSAGES && \
 cp ChangeLog ${DIR}/ChangeLog && \
 cp README.linux_binary ${DIR}/ReadMe && \
 cp src/.libs/${PROJECT}.so ${DIR}/plugins && \
 cp share/pixmaps/*.png ${DIR}/pixmaps/pidgin/${PROJECT} && \
 cp share/sounds/*.wav ${DIR}/sounds/pidgin/${PROJECT} && \
-cp po/de.gmo ${DIR}/locale/de/LC_MESSAGES/${PROJECT}.mo && \
-cp po/ru.gmo ${DIR}/locale/ru/LC_MESSAGES/${PROJECT}.mo && \
-cp po/fr.gmo ${DIR}/locale/fr/LC_MESSAGES/${PROJECT}.mo && \
+for lang in de ru fr es; do
+        mkdir -p ${DIR}/locale/${lang}/LC_MESSAGES && \
+        cp po/${lang}.gmo ${DIR}/locale/${lang}/LC_MESSAGES/${PROJECT}.mo
+done && \
 cp install_linux_package.sh ${DIR}/install.sh && \
 tar czvf ${DIR}.tar.gz ${DIR} && \
 rm -rf ${DIR}
--- build-win32-package.sh
+++ build-win32-package.sh
@@ -6,18 +6,16 @@ WIN32DIR=${PROJECT}-`cat VERSION`-win32 && \
 mkdir -p ${WIN32DIR}/pidgin/plugins && \
 mkdir -p ${WIN32DIR}/pidgin/pixmaps/pidgin/${PROJECT} && \
 mkdir -p ${WIN32DIR}/pidgin/sounds/pidgin/${PROJECT} && \
-mkdir -p ${WIN32DIR}/pidgin/locale/de/LC_MESSAGES && \
-mkdir -p ${WIN32DIR}/pidgin/locale/ru/LC_MESSAGES && \
-mkdir -p ${WIN32DIR}/pidgin/locale/fr/LC_MESSAGES && \
 sed 's/$/\r/' ChangeLog >${WIN32DIR}/ChangeLog.txt && \
 sed 's/$/\r/' README.win32 >${WIN32DIR}/ReadMe.txt && \
 cp src/${PROJECT}.dll ${WIN32DIR}/pidgin/plugins && \
 cp share/pixmaps/*.png ${WIN32DIR}/pidgin/pixmaps/pidgin/${PROJECT} && \
 cp share/sounds/*.wav ${WIN32DIR}/pidgin/sounds/pidgin/${PROJECT} && \
 i586-mingw32msvc-strip --strip-unneeded ${WIN32DIR}/pidgin/plugins/${PROJECT}.dll && \
-cp po/de.gmo ${WIN32DIR}/pidgin/locale/de/LC_MESSAGES/${PROJECT}.mo && \
-cp po/ru.gmo ${WIN32DIR}/pidgin/locale/ru/LC_MESSAGES/${PROJECT}.mo && \
-cp po/fr.gmo ${WIN32DIR}/pidgin/locale/fr/LC_MESSAGES/${PROJECT}.mo && \
+for lang in de ru fr es; do
+	mkdir -p ${WIN32DIR}/pidgin/locale/${lang}/LC_MESSAGES && \
+	cp po/${lang}.gmo ${WIN32DIR}/pidgin/locale/${lang}/LC_MESSAGES/${PROJECT}.mo
+done && \
 rm -f ${WIN32DIR}.zip && \
 cd ${WIN32DIR} && \
 zip -r ../${WIN32DIR}.zip * && \
