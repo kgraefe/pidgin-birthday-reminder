0c5712c64e87af9f0e384e13a69a507264b026fc
Konrad Gräfe <kgraefe@ponyslaystation>
2010-05-07 10:18:56 +0200

    * made change_version.sh and po-update.sh to search languages by themselves

--- po-update.sh
+++ po-update.sh
@@ -2,19 +2,9 @@
 cd po
 echo "Updating POT template..."
 intltool-update -po
-echo "Checking German language file..."
-intltool-update de
-echo "Checking Russian language file..."
-intltool-update ru 
-echo "Checking French language file..."
-intltool-update fr
-echo "Checking Spanish language file..."
-intltool-update es
-echo "Checking Turkish language file..."
-intltool-update tr
-echo "Checking Hebrew language file..."
-intltool-update he
-echo "Checking Portuguese language file..."
-intltool-update pt
-echo "Checking Galician language file..."
-intltool-update gl
+
+for f in *.po
+do echo "Checking ${f%.po} language file..."
+intltool-update ${f%.po}
+done
+
