2c9ca2a2c1b49050fe67ba623e504157f627ae83
Konrad Gräfe <konradgraefe@aol.com>
2015-12-20 14:04:40 -0800

    Update autotools scripts

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 set -x
 
 test -f VERSION || exit
-test -f configure.in.in || exit
+test -f configure.ac.in || exit
 test -f COPYING || exit
 
 languages=""
@@ -10,11 +10,16 @@ for f in po/*.po
 do test -f $f && languages="$languages $(basename $f .po)"
 done
 
-sed -e "s/@@VERSION@@/$(cat VERSION)/" -e "s/@@LANGUAGES@@/$(echo $languages)/" configure.in.in >configure.in || exit
+sed \
+    -e "s/@@VERSION@@/$(cat VERSION)/" \
+    -e "s/@@LANGUAGES@@/$(echo $languages)/" \
+configure.ac.in >configure.ac || exit
+
 aclocal || exit
 autoheader || exit
 libtoolize --copy || exit
-automake --add-missing --copy || exit
+automake --add-missing --copy --no-force || exit
 autoconf || exit
 libtoolize --copy --install || exit
 intltoolize --copy --force  || exit
+aclocal || exit
