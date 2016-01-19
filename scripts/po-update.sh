634937825c37ce28946a4691aed16cce9650b5ae
Konrad Gräfe <konradgraefe@aol.com>
2016-01-20 00:05:15 +0100

    Update scripts/po-update.sh
    We do not re-generate *.po files anymore since transifex does that now
    (I think).

--- scripts/po-update.sh
+++ scripts/po-update.sh
@@ -4,9 +4,3 @@ cd "$(dirname "$0")/../po"
 
 echo "Updating POT template..."
 intltool-update --pot --gettext-package=pidgin-birthday-reminder
-
-for f in *.po; do
-	echo "Checking ${f%.po} language file..."
-	intltool-update ${f%.po}
-done
-
