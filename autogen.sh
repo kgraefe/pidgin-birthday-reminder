df1e429a499128b37d2af8e500467c5bd13736e1
Konrad Gräfe <konradgraefe@aol.com>
2017-02-14 00:23:04 +0100

    Add header files to source tarball automatically
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- autogen.sh
+++ autogen.sh
@@ -3,10 +3,20 @@
 set +x
 set -e
 
-test -f VERSION
 test -f COPYING
+test -f Makefile.am.in
 
 ./scripts/gen-configure_ac.sh
+
+headers=""
+for f in src/*.h; do
+    test -f $f && headers="$headers $f"
+done
+
+sed \
+    -e "s#@@HEADERFILES@@#$(echo $headers)#" \
+Makefile.am.in >Makefile.am
+
 
 mkdir -p m4
 intltoolize --force --copy --automake
