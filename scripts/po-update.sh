1e59c2fb2c1c12baecf002ac2fed24d07e3af082
Konrad Gräfe <konradgraefe@aol.com>
2017-03-03 21:59:12 +0100

    po-update.sh: Fix C-style multiline comments
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- scripts/po-update.sh
+++ scripts/po-update.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+set -e
+
 cd "$(dirname "$0")/../po"
 
 echo "Updating POT template..."
 intltool-update --pot --gettext-package=pidgin-birthday-reminder
+
+# Fix C-Style multi-line comments
+sed -i \
+	-e 's/^#\. \* /#. /' \
+	-e '/^#\.$/d' \
+	pidgin-birthday-reminder.pot
